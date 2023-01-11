3732dc80cf4373326af21483328765cf44322e59
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-01-11 22:30:28 +0700

    Maulana Sodiqin | Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react'
+function App() {
+  return (
+    <Fragment>
+      <h1 className="text-red-400 font-sans text-4xl font-bold">Hallo Gaes</h1>
+    </Fragment>
+  )
+}
+
+export default App
